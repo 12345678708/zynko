5d413d22ea4e56c17fc6fcb0187195c09f17bef7
12345678708 <mordie7314@gmail.com>
2026-03-12 07:57:33 +0100

    Implement chat functionality with message loading

---
+++ static/script.js
@@ -0,0 +1,39 @@
+let currentFriend = null
+
+function openChat(friend){
+
+currentFriend = friend
+
+loadMessages()
+
+setInterval(loadMessages,2000)
+
+}
+
+function loadMessages(){
+
+fetch("/messages/"+currentFriend)
+
+.then(r=>r.json())
+
+.then(data=>{
+
+let box = document.getElementById("messages")
+
+box.innerHTML=""
+
+data.forEach(m=>{
+
+let div=document.createElement("div")
+
+div.className="bubble"
+
+div.innerText=m[0]+" : "+m[1]
+
+box.appendChild(div)
+
+})
+
+})
+
+}
