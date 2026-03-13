2c9d5e642c853e4f132e18bf389d508e0f3f1ca8
12345678708 <mordie7314@gmail.com>
2026-03-13 17:50:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -244,4 +244,4 @@ def logout():
 # ---------------- RUN ----------------
 
 if __name__ == "__main__":
-    socketio.run(app,host="0.0.0.0",port=10000)
+    socketio.run(app, host="0.0.0.0", port=5000)
