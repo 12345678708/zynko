364c0e5f11a7fe50af3efb23ed1f5cd19365216b
12345678708 <mordie7314@gmail.com>
2026-03-18 20:26:15 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,10 +2,11 @@ const chat=document.getElementById("chat");
 
 function send(){
 let text=document.getElementById("msg").value;
+let target=document.getElementById("target").value;
 
 socket.emit("send_message",{
 sender:USER,
-receiver:"global",
+receiver:target,
 text:text
 });
 }
@@ -13,32 +14,6 @@ text:text
 socket.on("new_message",data=>{
 let div=document.createElement("div");
 div.className="msg "+(data.sender===USER?"me":"other");
-
-div.innerHTML = data.text + 
-` <span onclick="react('${data.text}','❤️')">❤️</span>`;
-
+div.innerText=data.sender+" : "+data.text;
 chat.appendChild(div);
 });
-
-// ❤️ reaction
-function react(msg,emoji){
-socket.emit("react",{msg,emoji,user:USER});
-}
-
-socket.on("reaction",data=>{
-console.log("reaction",data);
-});
-
-// 📞 CALL
-function call(){
-socket.emit("call",{from:USER});
-}
-
-socket.on("incoming_call",data=>{
-alert("📞 Appel de "+data.from);
-});
-
-// 🌙
-function toggleDark(){
-document.body.classList.toggle("dark");
-}
