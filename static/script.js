bde75522bbc4e26193f7ddf541fd52e8e76c3a35
12345678708 <mordie7314@gmail.com>
2026-03-26 18:21:47 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,103 +1,48 @@
-const socket = io();
-let room = null;
+var socket = io();
 
-// NOTIFICATIONS
-if(Notification.permission !== "granted"){
-    Notification.requestPermission();
+let currentUser = null;
+
+function selectUser(user){
+    currentUser = user;
+    document.getElementById("chatWith").innerText = user;
+    document.getElementById("messages").innerHTML = "";
 }
 
-function notify(text){
-    if(Notification.permission==="granted"){
-        new Notification("Zynko",{body:text});
-    }
-}
-
-// ROOMS
-function createRoom(){
-    let name=prompt("Nom");
-    if(!name) return;
-
-    let div=document.createElement("div");
-    div.innerText=name;
-    div.onclick=()=>joinRoom(name);
-
-    document.getElementById("rooms").appendChild(div);
-}
-
-function joinRoom(name){
-    room=name;
-    document.getElementById("header").innerText=name;
-    socket.emit("join",{room:name});
-}
-
-// SEND
 function send(){
+    let input = document.getElementById("msg");
+    let text = input.value;
 
-    let msg=document.getElementById("msg").value;
-    let file=document.getElementById("file").files[0];
+    if(!text || !currentUser) return;
 
-    if(file){
-        let form=new FormData();
-        form.append("file",file);
-
-        fetch("/upload",{method:"POST",body:form})
-        .then(r=>r.json())
-        .then(data=>{
-            socket.emit("send",{room:room,image:data.url});
-        });
-    }
-
-    if(msg){
-        socket.emit("send",{room:room,text:msg});
-    }
-
-    document.getElementById("msg").value="";
-}
-
-// RECEIVE
-socket.on("msg",(data)=>{
-
-    let div=document.createElement("div");
-    div.className="bubble";
-
-    if(data.text){
-        div.innerText=data.text;
-    }
-
-    if(data.image){
-        let img=document.createElement("img");
-        img.src=data.image;
-        img.style.maxWidth="200px";
-        div.appendChild(img);
-    }
-
-    document.getElementById("messages").appendChild(div);
-
-    notify(data.text || "Image reçue");
-});
-
-// AUDIO
-function record(){
-    navigator.mediaDevices.getUserMedia({audio:true})
-    .then(stream=>{
-        const recorder=new MediaRecorder(stream);
-        let chunks=[];
-
-        recorder.ondataavailable=e=>chunks.push(e.data);
-
-        recorder.onstop=()=>{
-            let blob=new Blob(chunks);
-            let url=URL.createObjectURL(blob);
-
-            let audio=document.createElement("audio");
-            audio.src=url;
-            audio.controls=true;
-
-            document.getElementById("messages").appendChild(audio);
-        };
-
-        recorder.start();
-
-        setTimeout(()=>recorder.stop(),3000);
+    socket.emit("send_message", {
+        text: text,
+        to: currentUser
     });
+
+    input.value="";
 }
+
+function handleKey(e){
+    if(e.key === "Enter"){
+        send();
+    }
+}
+
+socket.on("new_message", function(data){
+
+    if(data.to !== currentUser) return;
+
+    let box = document.getElementById("messages");
+
+    let div = document.createElement("div");
+    div.className = "msg";
+
+    if(data.me){
+        div.classList.add("me");
+    }
+
+    div.innerText = data.text;
+
+    box.appendChild(div);
+    box.scrollTop = box.scrollHeight;
+});
