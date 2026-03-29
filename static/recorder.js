70ceb4e13bc90594d674357c4015aa69b9020b74
12345678708 <mordie7314@gmail.com>
2026-03-29 17:18:59 +0200

    Update recorder.js

--- static/recorder.js
+++ static/recorder.js
@@ -1,1 +1,22 @@
+let recorder, audioChunks
 
+async function record(){
+    let stream = await navigator.mediaDevices.getUserMedia({audio:true})
+    recorder = new MediaRecorder(stream)
+
+    recorder.ondataavailable = e => audioChunks.push(e.data)
+
+    recorder.onstop = ()=>{
+        let blob = new Blob(audioChunks)
+        let reader = new FileReader()
+        reader.onload = ()=>{
+            socket.emit("voice",{user:"me",audio:reader.result})
+        }
+        reader.readAsDataURL(blob)
+    }
+
+    audioChunks = []
+    recorder.start()
+
+    setTimeout(()=>recorder.stop(), 3000)
+}
