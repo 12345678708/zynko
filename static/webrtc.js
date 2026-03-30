a089c115e014f60de2833334af5d11bcbad75ed1
12345678708 <mordie7314@gmail.com>
2026-03-30 17:15:06 +0200

    Delete static/webrtc.js

--- static/webrtc.js
+++
@@ -1,6 +0,0 @@
-let pc = new RTCPeerConnection()
-
-async function startCall(){
-    let stream = await navigator.mediaDevices.getUserMedia({video:true,audio:true})
-    stream.getTracks().forEach(t=>pc.addTrack(t, stream))
-}
