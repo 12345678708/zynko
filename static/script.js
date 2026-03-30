baa07dd79ca544e9a5c3ca6f5d4df13c7aceb047
12345678708 <mordie7314@gmail.com>
2026-03-30 17:18:23 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
 let socket = io();
-
-let user = prompt("Pseudo");
+let user = prompt("Ton pseudo");
 
 socket.emit("join", {user:user});
 
+// ===== ENVOI TEXTE =====
 function send(){
     let msg = document.getElementById("msg").value;
 
@@ -15,22 +15,54 @@ function send(){
     document.getElementById("msg").value="";
 }
 
+// ===== AFFICHAGE =====
 socket.on("message", data=>{
     let div = document.createElement("div");
     div.className = "msg";
 
     if(data.user === user) div.classList.add("me");
 
-    div.innerText = data.user + " : " + data.text;
+    if(data.text){
+        div.innerText = data.user + " : " + data.text;
+    }
+
+    if(data.image){
+        div.innerHTML = `<b>${data.user}</b><br><img src="/static/uploads/${data.image}" width="150">`;
+    }
+
+    if(data.audio){
+        div.innerHTML = `<b>${data.user}</b><br><audio controls src="${data.audio}"></audio>`;
+    }
 
     document.getElementById("chat").appendChild(div);
 });
+
+// ===== IMAGE =====
+function sendImage(){
+    let file = document.getElementById("img").files[0];
+
+    let formData = new FormData();
+    formData.append("file", file);
+
+    fetch("/upload", {
+        method:"POST",
+        body:formData
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        socket.emit("message", {
+            user:user,
+            image:data.file
+        });
+    });
+}
+
+// ===== VOCAL =====
 let mediaRecorder;
 let audioChunks = [];
 
 async function startRecording(){
     let stream = await navigator.mediaDevices.getUserMedia({audio:true});
-
     mediaRecorder = new MediaRecorder(stream);
 
     mediaRecorder.ondataavailable = e=>{
