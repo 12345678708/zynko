121e2ca2dfea0e0b320a8bfca5513e5d2782bc42
12345678708 <mordie7314@gmail.com>
2026-04-02 17:08:40 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -30,8 +30,55 @@ socket.on("new_message", data=>{
         div.innerHTML = `<img src="/static/uploads/${data.image}" width="150">`;
     }
 
+    if(data.audio){
+        div.innerHTML = `<audio controls src="/static/uploads/${data.audio}"></audio>`;
+    }
+
     document.getElementById("chat").appendChild(div);
 });
+
+// ===== IMAGE =====
+function sendImage(){
+    let file = document.getElementById("img").files[0];
+    let form = new FormData();
+    form.append("file", file);
+
+    fetch("/upload", {method:"POST", body:form})
+    .then(r=>r.json())
+    .then(d=>{
+        socket.emit("send_message", {
+            user:user,
+            image:d.file
+        });
+    });
+}
+
+// ===== AUDIO =====
+let recorder;
+async function startAudio(){
+    let stream = await navigator.mediaDevices.getUserMedia({audio:true});
+    recorder = new MediaRecorder(stream);
+    recorder.start();
+
+    recorder.ondataavailable = async e=>{
+        let blob = e.data;
+
+        let form = new FormData();
+        form.append("audio", blob, "audio.webm");
+
+        let res = await fetch("/upload_audio", {method:"POST", body:form});
+        let data = await res.json();
+
+        socket.emit("send_message", {
+            user:user,
+            audio:data.audio
+        });
+    };
+}
+
+function stopAudio(){
+    recorder.stop();
+}
 
 // ===== TYPING =====
 function typing(){
@@ -42,21 +89,7 @@ function stopTyping(){
     socket.emit("stop_typing", {user:user});
 }
 
-// ===== IMAGE =====
-function sendImage(){
-    let file = document.getElementById("img").files[0];
-    let form = new FormData();
-    form.append("file", file);
-
-    fetch("/upload", {
-        method:"POST",
-        body:form
-    })
-    .then(r=>r.json())
-    .then(d=>{
-        socket.emit("send_message", {
-            user:user,
-            image:d.file
-        });
-    });
+// ===== NOTIFICATION =====
+if("Notification" in window){
+    Notification.requestPermission();
 }
