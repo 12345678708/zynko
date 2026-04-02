0fca6cc3aecd72ae78c7cd3103efbd098f3fd00f
12345678708 <mordie7314@gmail.com>
2026-04-02 17:09:15 +0200

    Update webrtc.js

--- static/webrtc.js
+++ static/webrtc.js
@@ -1,1 +1,26 @@
+let peer = new RTCPeerConnection();
 
+async function startCall(){
+    let stream = await navigator.mediaDevices.getUserMedia({video:true,audio:true});
+    document.getElementById("local").srcObject = stream;
+
+    stream.getTracks().forEach(track => peer.addTrack(track, stream));
+
+    let offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+
+    socket.emit("call", offer);
+}
+
+socket.on("call", async offer=>{
+    await peer.setRemoteDescription(offer);
+
+    let answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+
+    socket.emit("answer", answer);
+});
+
+socket.on("answer", async answer=>{
+    await peer.setRemoteDescription(answer);
+});
