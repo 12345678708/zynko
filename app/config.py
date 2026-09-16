7a66584f9efbc37bebdb9d1881180b5aac8c7a74
12345678708 <mordie7314@gmail.com>
2026-09-16 07:17:49 +0200

    Delete app directory

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-    # Default to an absolute path in /tmp which is writable in most hosts. In production, set DATABASE_URL env var (Postgres recommended).
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:////tmp/zynko.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "False") == "True"
-    PREFERRED_URL_SCHEME = "https"
