aaa1d1f270a668977d0d8aa39f540743f8ee0a82
12345678708 <mordie7314@gmail.com>
2026-09-16 07:20:29 +0200

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-# WSGI entrypoint expected by Procfile/gunicorn: "app:app"
-from app import app, socketio
-
-if __name__ == "__main__":
-    # local dev: run with socketio.run to enable Socket.IO
-    # In production Gunicorn with an async worker (eventlet/gevent) should be used.
-    socketio.run(app, host="0.0.0.0", port=10000, debug=True)
